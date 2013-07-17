b5aeda8a39c0ea0bd420ae5357ba4095374846a7
James Punnett <james.punnett@cibcfcib.com>
2013-07-17 17:21:12 -0400

    Saves convo to a file

--- lib/tcpproxy.rb
+++ lib/tcpproxy.rb
@@ -1,5 +1,5 @@
 require "tcpproxy/version"
-require 'methadone'
+require "methadone"
 require "gserver"
 require "socket"
 
@@ -18,24 +18,35 @@ module Tcpproxy
 
     def serve(io)
       host, port = get_endpoint_parts
+      ep = TCPSocket.new(host, port)
       
       begin
-        ep = TCPSocket.new(host, port)
+        ConvoLogger.new do |l|
         
-        # Copy data from client to endpoint
-        cli_2_ep = Thread.new(io, ep) do |from, to|
-          copy_sock_data(from, to)
-        end
+          cli_peeraddr = io.peeraddr[3] + ":" + io.peeraddr[1].to_s
+          ep_peeraddr = ep.peeraddr[3] + ":" + ep.peeraddr[1].to_s
 
-        # Copy data from endpoint to client
-        ep_2_cli = Thread.new(ep, io) do |from, to|
-          copy_sock_data(from, to)
+          debug("Convo starting: #{cli_peeraddr} <--> #{ep_peeraddr}")
+          l.log("Convo starting: #{cli_peeraddr} <--> #{ep_peeraddr}")
+          
+          # Copy data from client to endpoint
+          cli_2_ep = Thread.new(io, ep, l) do |from, to, logger|
+            copy_sock_data(from, to, l)
+          end
+
+          # Copy data from endpoint to client
+          ep_2_cli = Thread.new(ep, io, l) do |from, to, logger|
+            copy_sock_data(from, to, l)
+          end
+          
+          cli_2_ep.join
+          ep_2_cli.join
+          
+          debug("Convo ending: #{cli_peeraddr} <--> #{ep_peeraddr}")
+          l.log("Convo ending: #{cli_peeraddr} <--> #{ep_peeraddr}")
+          
         end
-        
-        cli_2_ep.join
-        ep_2_cli.join
       rescue => ex
-        debug("Failed to connect to #{@endpoint_name}.")
         debug("#{ex}")
       ensure
         begin
@@ -47,7 +58,10 @@ module Tcpproxy
     
     private
 
-    def copy_sock_data(from_sock, to_sock)
+    def copy_sock_data(from_sock, to_sock, logger)
+      from_addr = from_sock.peeraddr[3] + ":" + from_sock.peeraddr[1].to_s
+      to_addr = to_sock.peeraddr[3] + ":" + to_sock.peeraddr[1].to_s
+      
       done = false
       while !done
         begin
@@ -56,11 +70,36 @@ module Tcpproxy
           done = true
         end
         to_sock << buf if buf
+        logger.log("#{from_addr} --> #{to_addr} (#{buf.length}): #{buf}") if buf
       end
     end
 
     def get_endpoint_parts
       @endpoint_name.split(':', 2)
     end
-  end
+  end # class ProxyServer
+  
+  ##
+  # This class represents a threadsafe file appender. We use it to log
+  # information about a TCP/IP conversation.
+  class ConvoLogger
+  
+    def initialize
+      raise "Must supply a block" unless block_given?
+      @the_log_file = File.new("convo.out.txt", "w")
+      @the_log_file_mtx = Mutex.new
+      begin
+        yield self
+      ensure
+        @the_log_file.close if @the_log_file
+      end      
+    end
+    
+    def log(the_text)
+      @the_log_file_mtx.synchronize do
+        @the_log_file.puts the_text
+      end
+    end
+  end # class ConvoLogger
+  
 end
